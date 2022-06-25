8836434fa8aa1b294f4263df40fdadb71c19d504
Khanya <sibulelekupelo@gmail.com>
2022-06-25 15:03:06 +0200

    returns and params

--- index.ts
+++ index.ts
@@ -1,8 +1,13 @@
 ///<reference path="src/defining-types.ts"/>
 const s = require('./src/defining-types');
+import {WindowStates} from './src/composing-types';
 
 console.log(s.getUser())
 
 s.displayUserName(s.getUser())
 
 s.getAge(s.getUser())
+
+const windowState: WindowStates = 'closed'
+console.log(windowState)
+
---
+++ src/composing-types.ts
@@ -0,0 +1,11 @@
+type  myBool = true | false;
+
+const myBool_: myBool = false;
+console.log(myBool_);
+
+
+export type WindowStates = "open" | "closed" | "minimized";
+// const  windowState: WindowStates = "hello";
+
+
+
