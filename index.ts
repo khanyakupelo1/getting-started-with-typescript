2b1c3888df4083c9ef32bdab751b7af7ff18423a
Khanya <sibulelekupelo@gmail.com>
2022-06-25 13:48:30 +0200

    returns and params

--- index.ts
+++ index.ts
@@ -1,1 +1,32 @@
-console.log("Hello TypeScript");
+console.log("Defining Interface");
+interface User{
+    name: string;
+    age: number;
+}
+
+const user: User = {
+    name: "John",
+    age: 30
+}
+
+console.log('Working types as params')
+
+const displayUserName = (user: User) => {
+    console.log(user.name);
+}
+
+displayUserName(user);
+
+
+console.log('Working types as return')
+
+const getAge = (user: User): number => {
+    return user.age;
+}
+
+const getUser = (): User => user;
+
+
+
+
+
